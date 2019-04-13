d0381ab7a2097da2d8f252fa078e8489fcd8f62e
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-14 03:14:02 +0530

    charts added

--- app/pods/components/session-graphs/component.js
+++ app/pods/components/session-graphs/component.js
@@ -2,6 +2,24 @@ import Component from '@ember/component';
 
 export default Component.extend({
   dataUri: '',
+  textSentimentData: [
+    {
+      label: "sad",
+      value: 100
+    },
+    {
+      label: "something",
+      value: 100
+    },
+    {
+      label: "something",
+      value: 100
+    },
+    {
+      label: "happy",
+      value: 100
+    }
+  ],
   actions: {
     didSnap(dataUri) {
       // Delivers a data URI when snapshot is taken.
--- app/pods/initiate/controller.js
+++ app/pods/initiate/controller.js
@@ -2,4 +2,17 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   selectedPatient: null,
+  sessionStarted: false,
+  actions: {
+    startSession() {
+      if (!this.sessionStarted) {
+        this.set('sessionStarted', true)
+      }
+    },
+    stopSession() {
+      if (this.sessionStarted) {
+        this.set('sessionStarted', false)
+      }
+    }
+  }
 });
