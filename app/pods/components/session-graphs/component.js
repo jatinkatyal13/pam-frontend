5b4ad23b4264031663d3f740d342198a94082ef7
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-14 03:01:49 +0530

    added camera

--- app/pods/components/session-graphs/component.js
+++ app/pods/components/session-graphs/component.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  dataUri: '',
+  actions: {
+    didSnap(dataUri) {
+      // Delivers a data URI when snapshot is taken.
+      this.set('dataUri', dataUri);
+    },
+    didError(error) {
+      // Fires when a WebcamError occurs.
+      console.error(error);
+    }
+  }
 });
