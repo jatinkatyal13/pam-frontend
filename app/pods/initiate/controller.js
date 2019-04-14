a16f877d0faf2f8a75699d51186abf2f6c73a1b3
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-14 06:02:58 +0530

    notifications added

--- app/pods/initiate/controller.js
+++ app/pods/initiate/controller.js
@@ -1,12 +1,18 @@
+import Ember from 'ember'
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  notify: Ember.inject.service(),
   selectedPatient: null,
   sessionStarted: false,
   actions: {
     startSession() {
       if (!this.sessionStarted) {
-        this.set('sessionStarted', true)
+        if (!this.selectedPatient) {
+          this.notify.alert('Cannot start without selecting patient')
+        } else {
+          this.set('sessionStarted', true)
+        }
       }
     },
     stopSession() {
