96e261a838c3bc006d3cc1b04ae771744851d73f
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-14 07:11:09 +0530

    graphs somehow working

--- app/pods/components/session-graphs/component.js
+++ app/pods/components/session-graphs/component.js
@@ -1,9 +1,43 @@
+import Ember from 'ember'
 import Component from '@ember/component';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
+  api: Ember.inject.service(),
   dataUri: '',
   messages: [],
-  textSentimentData: {
+  sentiments: [0.6, 0.7,],
+  emotions: [],
+  allEmotions: {"anticipation": 0.0, "joy": 0.0, "sadness": 0.0, "neutral": 1.0, "disgust": 0.0, "anger": 0.0, "surprise": 0.0, "fear": 0.0, "trust": 0.0},
+  messagesAsChat: Ember.computed('messages.@each', function() {
+    return this.messages.map(message => ({
+      t: message,
+      x: '',
+      isOwner: false
+    }))
+  }),
+  textSentimentData: Ember.computed('sentiments.@each', function() {
+    const positive_data = this.get('sentiments').map(x => x)
+    const negative_data = this.get('sentiments').map(x => 1-x)
+    const res = {
+      datasets: [
+        {
+          label: 'positive',
+          data: positive_data,
+          borderWidth: 1,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)'
+        },
+        {
+          label: 'negative',
+          data: negative_data,
+          borderWidth: 1,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)' 
+        }
+      ]
+    }
+    return res
+  }),
+  textEmotionData: {
     labels: ['Sad', 'Happy', 'Fear'],
     datasets: [{
       label: '# of Votes',
@@ -11,9 +45,20 @@ export default Component.extend({
       borderWidth: 1
     }]
   },
+  fetchResultTask: task(function*(text) {
+    const result = JSON.parse(yield this.api.post('ml/text', {
+      data: {
+        text
+      }
+    }))
+    this.set('emotions', result.emotions)
+    
+    this.get('sentiments').pushObject(result.sentiments.magnitude)
+  }),
   actions: {
     onResult(result) {
       this.get('messages').pushObject(result)
+      this.fetchResultTask.perform(result)
       console.log(result)
     },
     didSnap(dataUri) {
